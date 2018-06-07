8965a31d7d7ce4c7a823bc2a46577cd011da86d3
Vinicius Cainelli <vicainelli@gmail.com>
2018-06-07 19:18:06 -0400

    Create new repo based on simonswiss/tailwind-starter

--- vuejs/unit-converter/gulpfile.js
+++ vuejs/unit-converter/gulpfile.js
@@ -1,13 +1,52 @@
-var gulp = require('gulp');
-var autoprefixer = require('gulp-autoprefixer');
-gulp.task('css', function () {
-  var postcss = require('gulp-postcss');
-  var tailwindcss = require('tailwindcss');
+// project paths are set in package.json
+const paths = require("./package.json").paths;
 
-  return gulp.src('src/styles.css')
-    .pipe(postcss([
-      tailwindcss('./tailwind.js'),
-      require('autoprefixer'),
-    ]))
-    .pipe(gulp.dest('build/'));
+const gulp = require("gulp");
+const postcss = require("gulp-postcss");
+const purgecss = require("gulp-purgecss");
+const tailwindcss = require("tailwindcss");
+const browserSync = require("browser-sync").create();
+
+// Custom extractor for purgeCSS, to avoid stripping classes with `:` prefixes
+class TailwindExtractor {
+  static extract(content) {
+    return content.match(/[A-z0-9-:\/]+/g) || [];
+  }
+}
+
+// compiling tailwind CSS
+gulp.task("css", () => {
+  return gulp
+    .src(paths.src.css + "*.css")
+    .pipe(
+      postcss([tailwindcss(paths.config.tailwind), require("autoprefixer")])
+    )
+    .pipe(
+      purgecss({
+        content: [paths.dist.base + "*.html"],
+        extractors: [
+          {
+            extractor: TailwindExtractor,
+            extensions: ["html", "js"]
+          }
+        ]
+      })
+    )
+    .pipe(gulp.dest(paths.dist.css));
 });
+
+// browser-sync dev server
+gulp.task("serve", ["css"], () => {
+  browserSync.init({
+    server: {
+      baseDir: "./dist/"
+    }
+  });
+
+  gulp.watch(paths.src.css + "*.css", ["css"]);
+  gulp.watch(paths.config.tailwind, ["css"]);
+  gulp.watch(paths.dist.base + "*.html").on("change", browserSync.reload);
+});
+
+// default task
+gulp.task("default", ["serve"]);
